d37ef555739e1e4ad4f08514101dfe2097198911
Ruben Kraus <s80350@htw-dresden.de>
2024-03-10 16:54:19 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+
+    
+</template>
+<script setup lang="ts">
+type Team = {
+  name: string;
+  members: Array<string>;
+};
+
+const teams = ref<Array<Team>>([] as Array<Team>);
+</script>
