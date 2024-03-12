b9ae5f50320d382800b9183fcb3789c84cf91559
Ruben Kraus <s80350@htw-dresden.de>
2024-03-12 17:16:51 +0100

    removed supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase", "@pinia/nuxt"],
-  supabase: {
-    redirect: false,
-  },
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
 });
