4d623b8a4b203a4149e4c12128a61ac315ace009
Ruben Kraus <s80350@htw-dresden.de>
2024-03-13 11:00:48 +0100

    lol haha xd (:

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,45 @@
 <template>
   <h1 class="text-5xl/normal text-center mb-20">
-    Dein Beerpong Tournier <Icon name="fluent-emoji-flat:beer-mug" />
+    Dein Beerpong Turnier <Icon name="fluent-emoji-flat:beer-mug" />
   </h1>
   <TeamComponent />
   <PlayerComponent />
   <MatchComponent />
 </template>
 <script setup lang="ts">
+import { link } from "fs";
+
 type Team = {
   name: string;
   members: Array<string>;
 };
 
 const teams = ref<Array<Team>>([] as Array<Team>);
+
+definePageMeta({
+  title: "Beerpong Turnier",
+  description: "Erstelle Teams und ordne Spieler zu.",
+});
+useHead({
+  link: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+    { rel: "manifest", href: "/site.webmanifest" },
+  ],
+});
 </script>
