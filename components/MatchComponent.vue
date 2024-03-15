ea2e9744cc86a009e2a483a4d8af05bc90f50b26
Ruben Kraus <s80350@htw-dresden.de>
2024-03-15 15:07:22 +0100

    update

--- components/MatchComponent.vue
+++ components/MatchComponent.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1 class="text-5xl/normal mt-16">Matches <Icon name="arcticons:litmatch"/></h1>
+  <h1 class="text-5xl/normal mt-16">
+    Matches <Icon name="arcticons:litmatch" />
+  </h1>
   <div class="flex flex-col gap-3 ring-2 ring-gray-400 rounded-md p-2 mb-10">
     <div class="flex justify-between">
       <div class="flex flex-col gap-1">
@@ -36,6 +38,23 @@
       Match erstellen
     </Button>
   </div>
+  <div class="flex flex-col gap-3 opacity-75">
+    <h3 class="text-2xl">Match Vorschläge</h3>
+    <div class="flex flex-col gap-3">
+      <!-- Iterate Groups and teams per group, create Matchcard for teams in group. Defferenciate for Groups with teams count !== % 2 === 0  -->
+      <MatchCard
+        v-for="(match, index) in matchesPerGroup"
+        :nummer="index + 1"
+        :uuid="'group-' + index"
+        :team1="match.team1"
+        :team2="match.team2"
+        :winner="match.winner"
+        :is-draw="match.draw"
+        :missing-cups-team1="match.missingCupsTeam1"
+        :missing-cups-team2="match.missingCupsTeam2"
+      />
+    </div>
+  </div>
   <div class="flex flex-col gap-3" v-if="matches.length > 0">
     <h3 class="text-2xl">Alle Matches:</h3>
     <div class="flex flex-col gap-3">
@@ -58,10 +77,34 @@
 </template>
 
 <script lang="ts" setup>
+import type { Team } from "./TeamComponent.vue";
 import Button from "./ui/button/Button.vue";
 
 const store = useBeerStore();
-const { teams, matches } = storeToRefs(store);
+const { teams, matches, calculatedGroups } = storeToRefs(store);
+
+const createMatchesPerGroup = (group: { name: string; teams: Team[] }) => {
+  const teams = group.teams;
+  const matches = [];
+  for (let i = 0; i < teams.length; i++) {
+    for (let j = i + 1; j < teams.length; j++) {
+      matches.push({
+        team1: teams[i],
+        team2: teams[j],
+        winner: null,
+        draw: false,
+        missingCups: 0,
+      });
+    }
+  }
+  return matches;
+};
+const matchesPerGroup = ref();
+
+watch(calculatedGroups, (groups) => {
+  matchesPerGroup.value = groups.map(createMatchesPerGroup);
+  console.log(matchesPerGroup.value);
+});
 
 const team1 = ref("null");
 const team2 = ref("null");
